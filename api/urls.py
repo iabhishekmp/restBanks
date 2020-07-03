72d5d9c3f5a533df0bbc12aa1ad7c6677cfaddbf
abhip1912 <abhipatil1912@gmail.com>
2020-07-03 10:04:05 +0530

    bug fixed

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-# from .views import DetailView, ListView
+from .views import detailView, listView
 
 urlpatterns = {
-    # url(r'^ifsc/(?P<ifsc>[A-Za-z]{4}\w{7})$', DetailView.as_view()),
-    # url(r'^branches/(?P<city>.*)/(?P<bank>.*)$', ListView.as_view())
+    url(r'^ifsc/(?P<ifsc>[A-Za-z]{4}\w{7})$', detailView),
+    url(r'^branches/(?P<city>.*)/(?P<bank>.*)$', listView)
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
